26c05386af98c5e404766c01ac048a60a23a2be5
LaValentine <86903020+LaValentine@users.noreply.github.com>
2022-07-09 04:40:25 +0400

    Repository initialized

---
+++ src/main/java/lav/valentine/apigeneratingtoken/ApiGeneratingTokenApplication.java
@@ -0,0 +1,13 @@
+package lav.valentine.apigeneratingtoken;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiGeneratingTokenApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ApiGeneratingTokenApplication.class, args);
+    }
+
+}
---
+++ src/test/java/lav/valentine/apigeneratingtoken/ApiGeneratingTokenApplicationTests.java
@@ -0,0 +1,13 @@
+package lav.valentine.apigeneratingtoken;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiGeneratingTokenApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
